ad88d393bca2a5ae0a374f76f03c69844750667e
duyu <c_duoyun@outlook.com>
2019-03-26 17:00:37 +0800

    优化前的代码

--- js/slides.js
+++ js/slides.js
@@ -1,47 +1,38 @@
-setTimeout(function(){
-	$('.slidesPicture >img:nth-child(1)').css({
-		transform:'translateX(-100%)'
-	})
-	$('.slidesPicture > img:nth-child(2)').css({
-		transform: 'translateX(-100%)'
-	})
-	$('.slidesPicture > img:nth-child(1)').one('transitionend',function(xx){
-		$(xx.currentTarget).addClass('right').css({transform:'none'})
-	})
+
+
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+
+setInterval(()=>{
+  $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+    .one('transitionend', (xx)=>{
+      $(xx.currentTarget).removeClass('leave').addClass('enter')
+    })
+  $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
 },3000)
 
-setTimeout(function(){
-	$('.slidesPicture > img:nth-child(2)').css({
-		transform: 'translateX(-200%)'
-	})
-	$('.slidesPicture > img:nth-child(3)').css({
-		transform: 'translateX(-100%)'
-	})
-	$('.slidesPicture > img:nth-child(2)').one('transitionend',function(xx){
-		$(xx.currentTarget).addClass('right').css({transform: 'none'})
-	})
+setInterval(()=>{
+  $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
+    .one('transitionend', (xx)=>{
+      $(xx.currentTarget).removeClass('leave').addClass('enter')
+    })
+  $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
 },6000)
 
-setTimeout(function(){
-	$('.slidesPicture > img:nth-child(3)').css({
-		transform: 'translateX(-200%)'
-	})
-	$('.slidesPicture > img:nth-child(1)').css({
-		transform: 'translateX(-100%)'
-	})
-	$('.slidesPicture > img:nth-child(3)').one('transitionend',function(xx){
-		$(xx.currentTarget).addClass('right').css({transform: 'none'})
-	})
+setInterval(()=>{
+  $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
+    .one('transitionend', (xx)=>{
+      $(xx.currentTarget).removeClass('leave').addClass('enter')
+    })
+  $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
 },9000)
 
-setTimeout(function(){
-	$('.slidesPicture > img:nth-child(1)').css({
-		transform: 'translateX(-200%)'
-	})
-	$('.slidesPicture > img:nth-child(2)').css({
-		transform: 'translateX(-100%)'
-	})
-	$('.slidesPicture > img:nth-child(1)').one('transitionend',function(xx){
-		$(xx.currentTarget).addClass('right').css({transform: 'none'})
-	})
+setInterval(()=>{
+  $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+    .one('transitionend', (xx)=>{
+      $(xx.currentTarget).removeClass('leave').addClass('enter')
+    })
+  $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
 },12000)
+
