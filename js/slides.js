1b43e428377d0b0cc3f008b8fcd41b7ac531b7f7
duyu <c_duoyun@outlook.com>
2019-03-27 09:20:48 +0800

    commit

--- js/slides.js
+++ js/slides.js
@@ -10,19 +10,3 @@
 				$('.slidesPicture > img:nth-child(2)').removeClass('enter').addClass('current')
 				},3000)
 
-			setInterval(()=>{
-				$('.slidesPicture > img:nth-child(2)').removeClass('current').addClass('leave')
-					.one('transitionend', (xx)=>{
-					$(xx.currentTarget).removeClass('leave').addClass('enter')
-					})
-				$('.slidesPicture > img:nth-child(3)').removeClass('enter').addClass('current')
-				},6000)
-
-			setInterval(()=>{
-				$('.slidesPicture > img:nth-child(3)').removeClass('current').addClass('leave')
-					.one('transitionend', (xx)=>{
-					$(xx.currentTarget).removeClass('leave').addClass('enter')
-					})
-				$('.slidesPicture > img:nth-child(1)').removeClass('enter').addClass('current')
-				},9000)
-
