40f93f35351fa4efe7fc2152f7c21183fafbab7b
duyu <c_duoyun@outlook.com>
2019-03-27 09:41:32 +0800

    优化前代码

--- js/slides.js
+++ js/slides.js
@@ -17,3 +17,19 @@ setTimeout(()=>{
 		})
 	$('.slidesPicture > img:nth-child(3)').removeClass('enter').addClass('current')
 },6000)
+
+setTimeout(()=>{
+	$('.slidesPicture > img:nth-child(3)').removeClass('current').addClass('leave')
+		.one('transitionend', (e)=>{
+			$(e.currentTarget).removeClass('leave').addClass('enter')
+		})
+	$('.slidesPicture > img:nth-child(1)').removeClass('enter').addClass('current')
+},9000)
+
+setTimeout(()=>{
+	$('.slidesPicture > img:nth-child(1)').removeClass('current').addClass('leave')
+		.one('transitionend', (e)=>{
+			$(e.currentTarget).removeClass('leave').addClass('enter')
+		})
+	$('.slidesPicture > img:nth-child(2)').removeClass('enter').addClass('current')
+},12000)
