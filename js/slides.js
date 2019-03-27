b50502bdab2a806be6733b97aff382ca262270e1
duyu <c_duoyun@outlook.com>
2019-03-27 09:38:31 +0800

    commit

--- js/slides.js
+++ js/slides.js
@@ -1,12 +1,19 @@
-            $('.slidesPicture > img:nth-child(1)').addClass('current')
-			$('.slidesPicture > img:nth-child(2)').addClass('enter')
-			$('.slidesPicture > img:nth-child(3)').addClass('enter')
+$('.slidesPicture > img:nth-child(1)').addClass('current')
+$('.slidesPicture > img:nth-child(2)').addClass('enter')
 
-			setInterval(()=>{
-				$('.slidesPicture > img:nth-child(1)').removeClass('current').addClass('leave')
-					.one('transitionend', (xx)=>{
-					$(xx.currentTarget).removeClass('leave').addClass('enter')
-					})
-				$('.slidesPicture > img:nth-child(2)').removeClass('enter').addClass('current')
-				},3000)
+setTimeout(()=>{
+	$('.slidesPicture > img:nth-child(1)').removeClass('current').addClass('leave')
+		.one('transitionend', (e)=>{
+			$(e.currentTarget).removeClass('leave').addClass('enter')
+		})
+	$('.slidesPicture > img:nth-child(2)').removeClass('enter').addClass('current')
+},3000)
 
+			
+setTimeout(()=>{
+	$('.slidesPicture > img:nth-child(2)').removeClass('current').addClass('leave')
+		.one('transitionend', (e)=>{
+			$(e.currentTarget).removeClass('leave').addClass('enter')
+		})
+	$('.slidesPicture > img:nth-child(3)').removeClass('enter').addClass('current')
+},6000)
